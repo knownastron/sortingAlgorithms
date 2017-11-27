e9dd6495ad2be663dafc8f89a8b273ac99d5b151
knownastron <knownastron@gmail.com>
2017-11-26 18:18:46 -0800

    fixed the exports/imports problem in heapSort.js and the helper file

--- heapSort.js
+++ heapSort.js
@@ -1,16 +1,12 @@
-const sink = require('./sortingHelper');
-const swim = require('./sortingHelper');
-const exch = require('./sortingHelper');
-//const less = require('./sortingHelper');
+const helper = require('./sortingHelper.js');
 
 /*
 function exch(a, index1, index2) {
     let holder = a[index1];
     a[index1] = a[index2];
     a[index2] = holder;
-    return a;
 }
-*/
+
 
 function less(itemOne, itemTwo) {
     if (itemOne < itemTwo) {
@@ -19,27 +15,29 @@ function less(itemOne, itemTwo) {
         return false;
     }
 }
+*/
 
 function heapSort(inputArray) {
     let n = inputArray.length;
     let newArray = [0];
     newArray = newArray.concat(inputArray);
     for (let i = Math.floor(n/2); i >= 1; i--) {
-        sink(newArray, i, n);
+        helper.sink(newArray, i, n);
     }
     while (n > 1) {
-        exch(newArray, 1, n--);
-        sink(newArray, 1, n);
+        helper.exch(newArray, 1, n);
+        n--;
+        helper.sink(newArray, 1, n);
     }
      return (newArray.splice(1, newArray.length).toString());
 }
+
 let myString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 //let myString = 'CDBYXAIJKVWNOPEFGQRSZLMHTU';
 let myArray = [];
 for (let i = 0; i < myString.length; i++) {
     myArray.push(myString[i]);
 }
-
-console.log(myArray[2], myArray[10]);
-exch(myArray, 2, 10);
+console.log(myArray.toString());
+helper.exch(myArray, 1, 10);
 console.log(myArray.toString());
--- sortingHelper.js
+++ sortingHelper.js
@@ -1,12 +1,15 @@
-function exch(a, index1, index2) {
+exports.exch = function (a, index1, index2) {
+    //console.log(a);
+    console.log(index1, index2);
+    //console.log('inside helper', a[index1], a[index2]);
     let holder = a[index1];
     a[index1] = a[index2];
     a[index2] = holder;
-    return a;
-}
+
+};
 
 /*
-function exch(theArray, indexOne, indexTwo) {
+function exports.exch(theArray, indexOne, indexTwo) {
     let holder = theArray[indexOne];
     theArray[indexOne] = theArray[indexTwo];
     theArray[indexTwo] = holder;
@@ -14,32 +17,27 @@ function exch(theArray, indexOne, indexTwo) {
 }
 */
 
-function less(item1, item2) {
+exports.less = function (item1, item2) {
     if (item1 < item2) {
         return true;
     } else {
         return false;
     }
-}
+};
 
-function swim(a, index, n) {
+exports.swim = function (a, index, n) {
     while (Math.floor(index/2) >= 1 && a[Math.floor(index/2)] < a[index]) {
-            exch(a[Math.floor(index/2)], a[index]);
+            exports.exch(a[Math.floor(index/2)], a[index]);
             index = Math.floor(index/2);
     }
-}
+};
 
-function sink(a, index, n) {
+exports.sink = function (a, index, n) {
     while ((index * 2) <= n) {
         let biggerIndex = index * 2;
-        if (less(a[index * 2], a[index * 2 + 1]) && biggerIndex < n) biggerIndex++;
-        if (!less(a[index], a[biggerIndex])) break;
-        exch(a, index, biggerIndex);
+        if (exports.less(a[index * 2], a[index * 2 + 1]) && biggerIndex < n) biggerIndex++;
+        if (!exports.less(a[index], a[biggerIndex])) break;
+        exports.exch(a, index, biggerIndex);
         index = biggerIndex;
     }
-}
-
-module.exports = less;
-module.exports = exch;
-module.exports = swim;
-module.exports = sink;
+};
